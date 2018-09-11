a86a5f4ab2e904941113dd59c06b3ab2bab14fe6
Ngakk <ngakwastaken@gmail.com>
2018-09-11 17:55:01 -0500

    Pruebas de grid

---
+++ Assets/_root/Branch lo de enfrente prot/usable/Grid/VisualGrid.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos
+{
+    public class VisualGrid : MonoBehaviour
+    {
+        public int width;
+        public int height;
+        public GameObject[] candies; //Los prefabs de cada 'dulce' que va a haber
+        public GameObject gameZone;
+
+        private Grid grid;
+
+        //Testing
+        public Camera cam;
+        public GameObject p;
+        public int x, y, z;
+
+        // Use this for initialization
+        void Start()
+        {
+            grid = GetComponent<Grid>();
+            Vector3 cellSize = grid.cellSize;
+
+            for(int i = 0; i < candies.Length; i++)
+            {
+                PoolManager.MakePool(candies[i]);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Testing
+            p.transform.position = grid.GetCellCenterLocal(new Vector3Int(x, y, z));
+        }
+    }
+}
+
+/*TODO:
+    Que se pongan en el lugar que se    
+ */
