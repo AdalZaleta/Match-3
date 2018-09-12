0da925fbc3c3e04389d98815e468ebabd831e1d5
Retus001 <retus001@gmail.com>
2018-09-12 01:21:26 -0500

    Save System - Version 1.0
    Progress | 100%
    Not Reviewed

--- Assets/_root/AritosLairOfDeathCommits/Scripts/Manager_Saves.cs
+++ Assets/_root/AritosLairOfDeathCommits/Scripts/Manager_Saves.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,6 +12,11 @@ namespace Mangos
         public GameObject newFileUI;
         public GameObject saveList;
         public GameObject savePrefab;
+        public GameObject delPrefab;
+        public GameObject grid;
+        public GameObject MainMenu;
+        public GameObject InGame;
+        public GameObject Score;
         DirectoryInfo di;
 
         private string m_selectedSave;
@@ -29,6 +35,18 @@ namespace Mangos
             newFileUI.SetActive(_bool);
         }
 
+        public void ToggleMainMenu(bool _bool)
+        {
+            MainMenu.SetActive(_bool);
+            InGame.SetActive(!_bool);
+        }
+
+        public void QuitCurrentGame()
+        {
+            ToggleMainMenu(true);
+            grid.GetComponent<Temp_GridSystem>().ClearGrid();
+        }
+
         // Creates empty txt file @ Resources/Saves with name given by input field
         public void CreateSavefile(InputField inputField)
         {
@@ -38,19 +56,109 @@ namespace Mangos
             LoadSaveList();
         }
 
+        public void DeleteSave(string _savename)
+        {
+            File.Delete("Assets/_root/Resources/Saves/" + _savename + ".txt");
+            LoadSaveList();
+        }
+
         // Gets data from game objects and stores them in it's corresponding savefile
         public void SaveGame()
         {
             // Get data from game
             // Save data to txt file with name
+
+            /*  SaveFile Format
+                
+             *  1st Line - Game Points
+             *  2nd Line - Grid Values
+                
+             */
+
+            using (var stream = new FileStream("Assets/_root/Resources/Saves/" + m_selectedSave + ".txt", FileMode.Truncate))
+            {
+                using (var clearWriter = new StreamWriter(stream))
+                {
+                    clearWriter.Write("");
+                }
+            }
+
+            StreamWriter writer = new StreamWriter("Assets/_root/Resources/Saves/" + m_selectedSave + ".txt", true);
+            writer.WriteLine(Score.GetComponent<Text>().text);
+
+            string gridValues = "";
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    if (i == 0 && j == 0)
+                        gridValues = gridValues + grid.GetComponent<Temp_GridSystem>().getGridValue(i, j);
+                    else
+                        gridValues = gridValues + " " + grid.GetComponent<Temp_GridSystem>().getGridValue(i, j);
+                }
+            }
+
+            writer.WriteLine(gridValues);
+            writer.Close();
         }
 
         // Loads data from corresponding txt file and sets them in-game
         public void LoadGame(string _savename)
         {
-            // Get Savename
-            // Get data from txt file named '_savename' from _root > Resources > Saves
-            // Set values in-game from savefile
+            m_selectedSave = _savename;
+
+            ToggleMainMenu(false);
+
+            if (new FileInfo("Assets/_root/Resources/Saves/" + m_selectedSave + ".txt").Length == 0)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 12; j++)
+                    {
+                        grid.GetComponent<Temp_GridSystem>().SetGridValue(i, j, (int)UnityEngine.Random.Range(1, 6));
+                    }
+                }
+                Score.GetComponent<Text>().text = "0";
+                grid.GetComponent<Temp_GridSystem>().InstantiateGrid();
+
+                SaveGame();
+            } else
+            {
+                StreamReader reader = new StreamReader("Assets/_root/Resources/Saves/" + m_selectedSave + ".txt");
+                int lineCounter = 0;
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    switch (lineCounter)
+                    {
+                        case 0:
+                            Score.GetComponent<Text>().text = line;
+                            break;
+
+                        case 1:
+                            int[] arrValues = Array.ConvertAll(line.Split(' '), int.Parse);
+
+                            int idValues = 0;
+                            for (int i = 0; i < 8; i++)
+                            {
+                                for (int j = 0; j < 12; j++)
+                                {
+                                    grid.GetComponent<Temp_GridSystem>().SetGridValue(i, j, arrValues[idValues]);
+                                    idValues++;
+                                }
+                            }
+                            grid.GetComponent<Temp_GridSystem>().InstantiateGrid();
+                            break;
+
+                        default:
+                            Debug.Log("Something Went Wrong");
+                            break;
+                    }
+                    lineCounter++;
+                }
+                reader.Close();
+            }
         }
 
         // Loads all savefile names from Resources/Saves and lists them
@@ -60,6 +168,12 @@ namespace Mangos
             // List saves using '_savename' as name as buttons
 
             // Get All Files in Resources/Saves Directory
+
+            foreach (Transform child in saveList.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+
             long _i = 0;
             FileInfo[] fis = di.GetFiles();
             foreach (FileInfo fi in fis)
@@ -71,7 +185,12 @@ namespace Mangos
                     save.GetComponent<RectTransform>().anchoredPosition = new Vector3(195, -115 - (50 * _i), 0); // Reposition Button
                     save.GetComponentInChildren<Text>().text = fi.Name.Split('.')[0]; // Rename Button
 
+                    GameObject del = Instantiate(delPrefab, saveList.transform);
+                    del.GetComponent<RectTransform>().anchoredPosition = new Vector3(335, -115 - (50 * _i), 0);
+
                     // Set OnClick Method to LoadGame(_savename) with _savename as the button's text value
+                    save.GetComponent<Button>().onClick.AddListener(delegate { LoadGame(save.GetComponentInChildren<Text>().text); });
+                    del.GetComponent<Button>().onClick.AddListener(delegate { DeleteSave(save.GetComponentInChildren<Text>().text); });
 
                     _i++;
                 }
---
+++ Assets/_root/AritosLairOfDeathCommits/Scripts/Temp_GridSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mangos
+{
+    public class Temp_GridSystem : MonoBehaviour
+    {
+
+        public int gridWidth;
+        public int gridHeight;
+        public int[,] grid;
+        public GameObject[] elements;
+        public GameObject gridStart;
+
+        private void Start()
+        {
+            grid = new int[gridWidth, gridHeight];
+        }
+
+        public void ClearGrid()
+        {
+            foreach (Transform child in gridStart.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+
+        private GameObject selectPrefab(int _id)
+        {
+            return elements[_id - 1];
+        }
+
+        public void InstantiateGrid()
+        {
+            for (int i = 0; i < gridWidth; i++)
+            {
+                for (int j = 0; j < gridHeight; j++)
+                {
+                    Instantiate(selectPrefab(grid[i, j]), new Vector3(gridStart.transform.position.x + i, gridStart.transform.position.y + j, 0), Quaternion.identity, gridStart.transform);
+                }
+            }
+        }
+
+        public void SetGridValue(int _x, int _y, int _value)
+        {
+            grid[_x, _y] = _value;
+        }
+
+        public int getGridValue(int _x, int _y)
+        {
+            return grid[_x, _y];
+        }
+    }
+}
