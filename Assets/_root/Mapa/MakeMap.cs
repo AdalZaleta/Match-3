c2a90b2a175b123f48c603d40129b1d8126c551a
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-09-13 08:51:38 -0500

    Arreglando Cosas

--- Assets/_root/Mapa/MakeMap.cs
+++ Assets/_root/Mapa/MakeMap.cs
@@ -21,11 +21,13 @@ public class MakeMap : MonoBehaviour {
 	public GameObject contenedorColumnas;
 	public GameObject contenedorFilas;
 	int[][] elemento;
+	bool[][] espejo;
 
 
 	void Start () 
 	{
 		elemento = new int[filas][];
+		espejo = new bool[filas][];
 
 		for(int i = 0 ; i < columnas; i++)
 		{
@@ -40,18 +42,91 @@ public class MakeMap : MonoBehaviour {
 		for(int w = 0 ; w < filas; w++)
 		{
 			elemento[w] = new int[columnas];
+			espejo[w] = new bool[columnas];
 			for(int x = 0 ; x < columnas; x++)
 			{
-				elemento[w][x] = Mathf.FloorToInt(Random.Range(0,3));
+				espejo[w][x] = false;
+				elemento[w][x] = Mathf.FloorToInt(Random.Range(1,4));
 			}
 		}
 
+		ViewElements();
+		ViewMatchs();
+	}
+
+	void ViewElements()
+	{
 		for(int w = 0 ; w < filas; w++)
 		{
 			for(int x = 0 ; x < columnas; x++)
 			{
-				Debug.Log("Elemento: " + elemento[w][x]);
+				Debug.Log("Elemento["+w+"]["+x+"]: " + elemento[w][x]);
 			}
 		}
-	} 
+	}
+
+	void ViewMatchs()
+	{
+		for(int w = 0 ; w < filas; w++)
+		{
+			for(int x = 0 ; x < columnas; x++)
+			{
+				Debug.Log("Valor de ["+w+"]["+x+"]: " + espejo[w][x]);
+			}
+		}
+	}
+
+	public void MakeMove(int _for, int _cor, int _fob, int _cob)
+	{
+		int temp = 0;
+		temp = elemento[_for][_cor];
+		elemento[_for][_cor] = elemento[_fob][_cob];
+		elemento[_fob][_cob] = temp;
+
+	}
+
+	public int GetElement(int fila, int columna)
+	{
+		return elemento[fila][columna];
+	}
+
+	public bool CheckMatch(int _for, int _cor)
+	{
+		if(_for == 0)
+		{
+			if(_cor == 0)
+			{
+				if(elemento[_for][_cor] == elemento[_for + 1][_cor  + 1])
+				{
+					if(elemento[_for][_cor] == elemento[_for + 2][_cor  + 2])
+					{
+						return true;
+					}
+				}
+			}
+			else if(_cor == columnas -1)
+			{
+
+			}
+			else
+			{
+
+			}
+		}
+		else if(_for == filas - 1)
+		{
+
+		}
+		else
+		{
+
+		}
+
+		return false;
+	}
+
+	public bool CheckMap()
+	{
+		return false;
+	}
 }
