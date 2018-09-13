bfa36c44a457a26f0c8d920df638b5b27067e7d4
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-09-13 07:15:30 -0500

    No se que coño hicé

---
+++ Assets/_root/Mapa/MakeItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeItem : MonoBehaviour {
+
+	public int Element;
+
+	void Awake()
+	{
+		Element = Random.Range(0,3);
+	}
+}
---
+++ Assets/_root/Mapa/MakeMap.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Elements
+{
+	FIRE,
+	WATER,
+	EARTH,
+	AIR	
+}
+
+public class MakeMap : MonoBehaviour {
+
+	[Range (1, 10)]
+	public int filas;
+	[Range (1, 10)]
+	public int columnas;
+	public Transform startPosition;
+	public GameObject pivotes;
+	public GameObject contenedorColumnas;
+	public GameObject contenedorFilas;
+	int[][] elemento;
+
+
+	void Start () 
+	{
+		elemento = new int[filas][];
+
+		for(int i = 0 ; i < columnas; i++)
+		{
+			GameObject.Instantiate(pivotes, new Vector3(startPosition.position.x, startPosition.position.y, startPosition.position.z + i * 1.0f + 0.5f), Quaternion.identity);
+		}
+
+		for(int j = 0 ; j < filas; j++)
+		{
+			GameObject.Instantiate(pivotes, new Vector3(startPosition.position.x + j * 1.0f + 0.5f, startPosition.position.y, startPosition.position.z), Quaternion.identity);
+		}
+
+		for(int w = 0 ; w < filas; w++)
+		{
+			elemento[w] = new int[columnas];
+			for(int x = 0 ; x < columnas; x++)
+			{
+				elemento[w][x] = Mathf.FloorToInt(Random.Range(0,3));
+			}
+		}
+
+		for(int w = 0 ; w < filas; w++)
+		{
+			for(int x = 0 ; x < columnas; x++)
+			{
+				Debug.Log("Elemento: " + elemento[w][x]);
+			}
+		}
+	} 
+}
