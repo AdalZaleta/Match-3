5d066172c530d100e39857829d618ffaadb7827c
Ngakk <ngakwastaken@gmail.com>
2018-10-04 08:40:16 -0500

    app manager bueno

--- Assets/_root/Managers/Manager_Audio.cs
+++ Assets/_root/Managers/Manager_Audio.cs
@@ -2,13 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Mangos
+namespace Mangos 
 {
+    public enum sounds : int
+    {
+        BUTTON,
+        LOSE,
+        MOVE,
+        NORMAL_MATCH,
+        SPECIAL_MATCH,
+        WIN
+    }
+
 	public class Manager_Audio : MonoBehaviour {
 
 		public GameObject audioDad;
 		public Camera cam;
 		public AudioClip mainMenu;
+
+        public AudioClip[] clips;
 
 		void Awake()
 		{
@@ -28,14 +40,28 @@ namespace Mangos
 			Destroy (sound, clip.length + 0.1f);
 		}
 
-		public void PlaySoundGlobal(AudioClip clip)
+        public void PlaySoundAt(Vector3 pos, sounds clip)
+        {
+            GameObject sound = Instantiate(audioDad, pos, Quaternion.identity);
+            sound.GetComponent<AudioSource>().PlayOneShot(clips[(int)clip]);
+            Destroy(sound, clips[(int)clip].length + 0.1f);
+        }
+
+        public void PlaySoundGlobal(AudioClip clip)
 		{
 			GameObject sound = Instantiate (audioDad, cam.transform.position, Quaternion.identity, cam.transform);
 			sound.GetComponent<AudioSource> ().PlayOneShot (clip);
 			Destroy (sound, clip.length + 0.1f);
 		}
 
-		public void PlayMusic(AudioClip clip)
+        public void PlaySoundGlobal(sounds clip)
+        {
+            GameObject sound = Instantiate(audioDad, cam.transform.position, Quaternion.identity, cam.transform);
+            sound.GetComponent<AudioSource>().PlayOneShot(clips[(int)clip]);
+            Destroy(sound, clips[(int)clip].length + 0.1f);
+        }
+
+        public void PlayMusic(AudioClip clip)
 		{
 			if (!GameObject.Find("jukebox"))
 			{
@@ -52,7 +78,24 @@ namespace Mangos
 			}
 		}
 
-		public void StopMusic()
+        public void PlayMusic(sounds clip)
+        {
+            if (!GameObject.Find("jukebox"))
+            {
+                GameObject jukebox = Instantiate(audioDad, cam.transform.position, Quaternion.identity, cam.transform);
+                jukebox.gameObject.name = "jukebox";
+                jukebox.GetComponent<AudioSource>().clip = clips[(int)clip];
+                jukebox.GetComponent<AudioSource>().loop = true;
+                jukebox.GetComponent<AudioSource>().Play();
+            }
+            else
+            {
+                GameObject.Find("jukebox").GetComponent<AudioSource>().clip = clips[(int)clip];
+                GameObject.Find("jukebox").GetComponent<AudioSource>().Play();
+            }
+        }
+
+        public void StopMusic()
 		{
 			if (GameObject.Find("jukebox"))
 			{
